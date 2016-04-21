87f1d2f189792f5577b5aeecd2367189114805a7
DILL , BENNETT <bennett.dill@kroger.com>
2016-04-21 12:43:47 -0400

    Added support for proxy pass-through, delayed responses, and error rates

--- src/api.js
+++ src/api.js
@@ -2,21 +2,68 @@ var http = require('http');
 var log = require('./message.js');
 var APIData = require('./apidata.js');
 var util = require('util');
+var request = require('request');
+var url = require('url');
+var fs = require('fs');
+
+function flatten(arr) {
+  return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+}
+
+function scanForStubs(path, endpointsConfig) {
+  var cfg = util.isArray(endpointsConfig) ?
+      JSON.parse(JSON.stringify(endpointsConfig)) : [];
+
+  try {
+    return flatten(cfg.concat(fs.readdirSync(path)
+        .filter(function (file) {
+          return file.substr(-3) === '.js';
+        })
+        .map(function (item) {
+          return require(path + '/' + item);
+        })));
+  }
+  catch (e) {
+    return cfg;
+  }
+}
+
+function proxyRequest(proxyUrl, req, res) {
+  var options = {
+    uri: proxyUrl + req.url,
+    headers: req.headers
+  };
+
+  options.headers['host'] = url.parse(options.uri).host;
+
+  var r = null;
+
+  if (req.method === 'POST') {
+    options.json = req.body;
+    r = request.post(options);
+  } else {
+    r = request(options);
+  }
+
+  req.pipe(r).pipe(res);
+}
 
 /*
-Check if setting object passed from API constructor
-has minimum information to run api. (path & data)
-Arg:
-  - endpintSetting (Object)
-  - index (Number)
-Return: state (Boolean)
-*/
-function verifyEndpointSettings(endpointSettings, index){
+ Check if setting object passed from API constructor
+ has minimum information to run api. (path & data)
+ Arg:
+ - endpointSetting (Object)
+ - index (Number)
+ Return: state (Boolean)
+ */
+function verifyEndpointSettings(endpointSettings, index) {
   var state = true;
-  if(!endpointSettings.path){
+  if (!endpointSettings.path) {
     state = false;
     log.pathFieldNotSpecified(index);
-  }else if(!endpointSettings.data){
+  } else if (!endpointSettings.data) {
     state = false;
     log.dataFieldNotSpecified(endpointSettings.path);
   }
@@ -32,44 +79,44 @@ function addHeaders(response, headers) {
 }
 
 /*
-API constructor
-On initialize, it will...
-  - parse through each endpoint setting to create a dataset
-  - create http server
+ API constructor
+ On initialize, it will...
+ - parse through each endpoint setting to create a dataset
+ - create http server
 
-Arguments:
-  - config (Array): array of endpoint setting objects
-  - or
-  - config (Object): object with 2 properties, defaultHeaders (array of header name value) and endpoints (array of endpoint setting objects)
+ Arguments:
+ - config (Array): array of endpoint setting objects
+ - or
+ - config (Object): object with 2 properties, defaultHeaders (array of header name value) and endpoints (array of endpoint setting objects)
 
-Return: Object with access to methods
-  - start(port number, [callback]): start http server on specified port number
-  - stop([callback]): stop http server
-  - profile(): send status of the API
-    Return:
-      - status(Boolean) : True if http server is running
-      - port(Number) : port number that http sever is using,
-      - running_since(DateTime): time the http server started,
-      - path(Array) : list of paths that is set for this API
-*/
-function API(config){
+ Return: Object with access to methods
+ - start(port number, [callback]): start http server on specified port number
+ - stop([callback]): stop http server
+ - profile(): send status of the API
+ Return:
+ - status(Boolean) : True if http server is running
+ - port(Number) : port number that http sever is using,
+ - running_since(DateTime): time the http server started,
+ - path(Array) : list of paths that is set for this API
+ */
+function API(config) {
+  if (!config) {
+    log.generalError('config is undefined');
+  }
+
   var portNum;
   var apiStatus = false;
   var startTime;
   var endpoints = {};
   var defaultHeaders = [];
-  var that = this;
 
-  var endpointsConfig;
-  if (util.isArray(config)) {
-    endpointsConfig = config;
-  } else {
-    endpointsConfig = config.endpoints;
-  }
+  const proxy = config.proxy;
+  const stubsPath = config.stubsPath || process.cwd() + '/stubs';
+  const endpointsConfig = scanForStubs(stubsPath, config.endpoints);
 
   // Parse endpointsConfig and create Endpoint object for each path
-  endpointsConfig.map(function(endpointSettings, i){
-    if(verifyEndpointSettings(endpointSettings, i)){
+  endpointsConfig.forEach(function (endpointSettings, i) {
+    if (verifyEndpointSettings(endpointSettings, i)) {
       endpoints[endpointSettings.path] = new APIData(endpointSettings);
     }
   });
@@ -79,58 +126,91 @@ function API(config){
     defaultHeaders = config.defaultHeaders;
   }
 
-    // Create http server
-  var server =  http.createServer(function (req, res) {
-    if(endpoints[req.url]){
-      addHeaders(res, defaultHeaders);
-      addHeaders(res, endpoints[req.url].headers);
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(endpoints[req.url].getJSONString());
-    } else {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
+  // Create http server
+  var server = http.createServer(function (req, res) {
+    if (!endpoints[req.url]) {
+      if (proxy) {
+        log.requestProxied(req.url);
+        proxyRequest(proxy, req, res);
+        return;
+      }
+
+      log.unknownPath(req.url);
+      res.writeHead(404, {'Content-Type': 'text/plain'});
       res.end('No Data Specified here');
+      return;
     }
+
+    if (endpoints[req.url].isError()) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('Error generated based on settings');
+      return;
+    }
+
+    // start the response
+    addHeaders(res, defaultHeaders);
+    addHeaders(res, endpoints[req.url].headers);
+    res.writeHead(200, {'Content-Type': 'application/json'});
+
+    var delay = endpoints[req.url].getDelay();
+
+    if (!delay) {
+      res.end(endpoints[req.url].getJSONString());
+      return;
+    }
+
+    setTimeout(function () {
+      res.end(endpoints[req.url].getJSONString());
+    }, delay);
   });
 
-  server.on('error', function(e){
+  server.on('error', function (e) {
     if (e.code == 'EADDRINUSE') {
       return log.portAlreadyUsed(portNum);
     }
     return log.generalError(e);
   });
 
-  function startAPI(port, callback){
-    if(apiStatus === true){return log.apiAlreadyRunning(portNum);}
-    if(port === undefined){ return log.portNotDefined();}
-    if(isNaN(port)){ return log.portNaN(port);}
+  function startAPI(port, callback) {
+    if (apiStatus === true) {
+      return log.apiAlreadyRunning(portNum);
+    }
+    if (port === undefined) {
+      return log.portNotDefined();
+    }
+    if (isNaN(port)) {
+      return log.portNaN(port);
+    }
     portNum = port;
-    server.listen(port,null,null,function(){
+    server.listen(port, null, null, function () {
       apiStatus = true;
       startTime = new Date();
       portNum = server.address().port;
       log.apiServerStarted(portNum);
-      if(callback){callback(true);}
-      return;
+      if (callback) {
+        callback(true);
+      }
     });
   }
 
-  function stopAPI(callback){
-    server.close(function(){
+  function stopAPI(callback) {
+    server.close(function () {
       log.apiServerStopped(portNum);
       apiStatus = false;
       startTime = undefined;
       portNum = undefined;
-      if(callback){callback(true);}
-      return;
+      if (callback) {
+        callback(true);
+      }
     });
   }
 
-  function profile(){
+  function profile() {
     return {
-      status : apiStatus,
-      port : portNum,
+      status: apiStatus,
+      port: portNum,
       running_since: startTime,
-      path : Object.keys(endpoints)
+      path: Object.keys(endpoints)
     };
   }
 
@@ -138,7 +218,7 @@ function API(config){
   return {
     start: startAPI,
     stop: stopAPI,
-    profile : profile,
+    profile: profile
   };
 }
 
--- test/test.js
+++ test/test.js
@@ -3,34 +3,34 @@ var API = require('../src/api.js');
 var APIData = require('../src/apidata.js');
 
 var endpointSetting = {
-  path:'/data',
-  data:{
-    log:['/// log*10 ///']
+  path: '/data',
+  data: {
+    log: ['/// log*10 ///']
   },
-  templates:{
-    log:{
-      id:{
-        stub_type:'number',
-        min_num:100,
-        max_num:500,
-        type:'random',
+  templates: {
+    log: {
+      id: {
+        stub_type: 'number',
+        min_num: 100,
+        max_num: 500,
+        type: 'random',
       },
-      amount:{
-        stub_type:'select',
-        choice:[100,200,300,400],
-        type:'random'
+      amount: {
+        stub_type: 'select',
+        choice: [100, 200, 300, 400],
+        type: 'random'
       },
-      text:{
-        stub_type:'lipsum',
-        min_char_length:100,
-        max_char_length:300,
+      text: {
+        stub_type: 'lipsum',
+        min_char_length: 100,
+        max_char_length: 300,
       },
-      date:{
-        stub_type:'datetime',
-        output_format:'toYMDString',
-        type:'random',
-        max_datetime:'2014-01-01',
-        min_datetime:'2014-01-01',
+      date: {
+        stub_type: 'datetime',
+        output_format: 'toYMDString',
+        type: 'random',
+        max_datetime: '2014-01-01',
+        min_datetime: '2014-01-01',
       }
     }
   }
@@ -38,9 +38,9 @@ var endpointSetting = {
 
 //test API server
 var apiserver = new API([]);
-apiserver.start(3000, function(success){
+apiserver.start(3000, function (success) {
   assert(success == true, 'Start server')
-  apiserver.stop(function(success){
+  apiserver.stop(function (success) {
     assert(success == true, 'Stop server')
   });
 });
@@ -51,6 +51,37 @@ var dataObj = JSON.parse(data.getJSONString())
 assert(dataObj.log.length == 10, 'length')
 assert(dataObj.log[0].id <= 500, 'max_num')
 assert(dataObj.log[0].id >= 100, 'min_num')
-assert([100,200,300,400].indexOf(dataObj.log[0].amount) != -1 , 'select')
+assert([100, 200, 300, 400].indexOf(dataObj.log[0].amount) != -1, 'select')
 assert(typeof(dataObj.log[0].text) == 'string', 'string')
 assert(dataObj.log[0].date == '2014-01-01', 'date')
+
+endpointSetting = {
+  path: 'fast',
+  data: {fast: true}
+};
+
+data = new APIData(endpointSetting);
+assert.equal(data.getDelay(), undefined);
+
+endpointSetting = {
+  path: 'slow',
+  data: {fast: false},
+  delay: {min: 200, max: 200}
+};
+
+data = new APIData(endpointSetting);
+assert(data.getDelay() === 200);
+
+endpointSetting = {
+  path: 'rnd',
+  data: {fast: false},
+  delay: {min: 50, max: 1000}
+};
+
+data = new APIData(endpointSetting);
+
+for (var i = 0; i < 1000; i++){
+  var delay = data.getDelay();
+  assert(delay >= endpointSetting.delay.min);
+  assert(delay <= endpointSetting.delay.max);
+}
